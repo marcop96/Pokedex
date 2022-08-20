94f542e71afde077c3ddf4f9c654c1c0162c6fd6
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-08-20 12:42:21 +0200

    add pokemon list to screen

---
+++ app.js
@@ -0,0 +1,34 @@
+const POKEMON_LIST = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
+const POKEMON_LIST_CONTAINER = document.querySelector(
+  "#pokemon-list-container"
+);
+const POKEMON_CARD_CONTAINER = document.querySelector(
+  "#pokemon-card-container"
+);
+
+let pokemonList;
+
+function getPokemonList() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+    .then((response) => response.json())
+    .then((data) => {
+      const {
+        count: pokemonCount,
+        next: nextURL,
+        previous: previousURL,
+        results: pokemonList,
+      } = data;
+
+      pokemonList.forEach(function (pokemon) {
+        createPokemonList(pokemon.name);
+      });
+    });
+}
+getPokemonList();
+
+function createPokemonList(pokemon) {
+  let createList = document.createElement("li");
+
+  createList.innerText = pokemon;
+  POKEMON_LIST_CONTAINER.appendChild(createList);
+}
